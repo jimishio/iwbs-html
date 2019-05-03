558f601afe171967e3b7c5179fce3f1fa6b3ef47
Jimish Parekh <jimishparekh123@gmail.com>
2019-05-03 11:51:41 +0530

    adding more details

--- js/custom.js
+++ js/custom.js
@@ -46,6 +46,7 @@ $(document).ready(function()
 	initHeaderSearch();
 	initHomeSlider();
 	initMilestones();
+	initAccordions();
 
 	/* 
 
@@ -62,6 +63,71 @@ $(document).ready(function()
 		else
 		{
 			header.removeClass('scrolled');
+		}
+	}
+
+	function initAccordions()
+	{
+		if($('.accordion').length)
+		{
+			var accs = $('.accordion');
+
+			accs.each(function()
+			{
+				var acc = $(this);
+
+				if(acc.hasClass('active'))
+				{
+					var panel = $(acc.next());
+					var panelH = panel.prop('scrollHeight') + "px";
+					
+					if(panel.css('max-height') == "0px")
+					{
+						panel.css('max-height', panel.prop('scrollHeight') + "px");
+					}
+					else
+					{
+						panel.css('max-height', "0px");
+					}
+					$(window).trigger('resize.px.parallax');
+				}
+
+				acc.on('click', function()
+				{
+					if(acc.hasClass('active'))
+					{
+						acc.removeClass('active');
+						var panel = $(acc.next());
+						var panelH = panel.prop('scrollHeight') + "px";
+						
+						if(panel.css('max-height') == "0px")
+						{
+							panel.css('max-height', panel.prop('scrollHeight') + "px");
+						}
+						else
+						{
+							panel.css('max-height', "0px");
+						}
+						$(window).trigger('resize.px.parallax');
+					}
+					else
+					{
+						acc.addClass('active');
+						var panel = $(acc.next());
+						var panelH = panel.prop('scrollHeight') + "px";
+						
+						if(panel.css('max-height') == "0px")
+						{
+							panel.css('max-height', panel.prop('scrollHeight') + "px");
+						}
+						else
+						{
+							panel.css('max-height', "0px");
+						}
+						$(window).trigger('resize.px.parallax');
+					}
+				});
+			});
 		}
 	}
 
